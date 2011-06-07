98b36fff77ff9a0ece712d233dc3721e798a51e9
Randall Degges <rdegges@gmail.com>
2011-06-07 14:01:40 -0700

    Adding define rabbitmq::ctl::start_app.

---
+++ manifests/ctl/start_app.pp
@@ -0,0 +1,15 @@
+# Define: rabbitmq::ctl::start_app
+#
+# Starts the RabbitMQ application. This command is typically run after
+# performing other management actions that required the RabbitMQ application to
+# be stopped, e.g. reset.
+#
+# Usage:
+# rabbitmq::ctl::start_app
+#
+define rabbitmq::ctl::start_app() {
+
+	Exec { path => "/usr/sbin" }
+	exec { "rabbitmqctl start_app" }
+
+}
