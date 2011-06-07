a072010bea361f10d802d74aa1a45ad5cb383d81
Randall Degges <rdegges@gmail.com>
2011-06-07 14:00:23 -0700

    Adding define rabbitmq::ctl::stop_app.

---
+++ manifests/ctl/stop_app.pp
@@ -0,0 +1,15 @@
+# Define: rabbitmq::ctl::stop_app
+#
+# Stops the RabbitMQ application, leaving the Erlang node running. This command
+# is typically run prior to performing other management actions that require
+# the RabbitMQ application to be stopped, e.g. reset.
+#
+# Usage:
+# rabbitmq::ctl::stop_app
+#
+define rabbitmq::ctl::stop_app() {
+
+	Exec { path => "/usr/sbin" }
+	exec { "rabbitmqctl stop_app" }
+
+}
