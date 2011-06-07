f11eac988778a04e9a4bb78e76fa77e9d2915b40
Randall Degges <rdegges@gmail.com>
2011-06-07 13:40:24 -0700

    Adding class rabbitmq::absent.
    This will uninstall rabbitmq from the system.

---
+++ manifests/absent.pp
@@ -0,0 +1,14 @@
+# Class: rabbitmq::absent
+#
+# Uninstalls rabbitmq.
+#
+# Usage:
+# include rabbitmq::absent
+#
+class rabbitmq::absent inherits rabbitmq {
+
+	Package["rabbitmq-server"] {
+		ensure	=> absent,
+	}
+
+}
