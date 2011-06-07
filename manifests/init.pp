fb0a113a432fe126ed0d6a0b855f6f52da809451
Randall Degges <rdegges@gmail.com>
2011-06-07 12:15:25 -0700

    Adding rabbitmq class.
    This class will install and run rabbitmq on a ubuntu system. For now, I'm going
    to assume the platform is ubuntu. Later on, once everything is finished and
    tested, I'll go through and add proper support for other operating systems.

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+#
+# Class: rabbitmq
+#
+# Installs and runs the rabbitmq messaging server: http://www.rabbitmq.com/.
+#
+# Usage:
+# include rabbitmq
+class rabbitmq {
+
+	# Ensure rabbitmq is installed:
+	package { "rabbitmq-server": ensure => present }
+
+	# Ensure rabbitmq is running:
+	service { "rabbitmq-server":
+		enable		=> true,
+		ensure		=> running,
+		hasrestart	=> true,
+		hasstatus	=> true,
+		require		=> Package["rabbitmq-server"],
+	}
+
+}
