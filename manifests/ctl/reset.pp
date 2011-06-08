f65799ec4da0e5a5f69986e334c90754d9c86377
Randall Degges <rdegges@gmail.com>
2011-06-07 17:04:38 -0700

    Adding rabbitmq::ctl::reset define.

---
+++ manifests/ctl/reset.pp
@@ -0,0 +1,16 @@
+# Define: rabbitmq::ctl::reset
+#
+# Return a RabbitMQ node to its virgin state. Removes the node from any cluster
+# it belongs to, removes all data from the management database, such as
+# configured users and vhosts, and deletes all persistent messages. For reset
+# and force_reset to succeed the RabbitMQ application must have been stopped,
+# e.g. with stop_app.
+#
+# Usage:
+# rabbitmq::ctl::reset
+#
+define rabbitmq::ctl::reset() {
+
+	exec { "reset": command => "/usr/sbin/rabbitmqctl reset" }
+
+}
