54b888cbd7e6d0edff1578ca8b07298016ae864f
Randall Degges <rdegges@gmail.com>
2011-06-07 17:02:11 -0700

    Adding rabbitmq::ctl::wait define.

---
+++ manifests/ctl/wait.pp
@@ -0,0 +1,15 @@
+# Define: rabbitmq::ctl::wait
+#
+# Wait for the RabbitMQ application to start. This command will wait for the
+# RabbitMQ application to start at the node. As long as the Erlang node is up
+# but the RabbitMQ application is down it will wait indefinitely. If the node
+# itself goes down, or takes more than five seconds to come up, it will fail.
+#
+# Usage:
+# rabbitmq::ctl::wait
+#
+define rabbitmq::ctl::wait() {
+
+	exec { "wait": command => "/usr/sbin/rabbitmqctl wait" }
+
+}
