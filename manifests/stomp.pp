1a1f1de034bc8a9fcd4bbfb038023e51cdf54706
Kris Buytaert <sdog@inuits.be>
2012-01-04 17:28:22 +0100

    Adding stomp support

---
+++ manifests/stomp.pp
@@ -0,0 +1,28 @@
+# Class: rabbitmq::stomp
+#
+#Enables Stomp
+#
+# Usage:
+# include rabbitmq::stomp
+#
+
+# Ugly hack using static files .. There most probably is a better approach to this 
+class rabbitmq::stomp {
+
+
+  file { '/etc/rabbitmq/enabled_plugins':
+    group  => '0',
+    mode   => '644',
+    owner  => '0',
+    source => 'puppet:///modules/rabbitmq/enabled_plugins';
+  }
+
+  file { '/etc/rabbitmq/rabbitmq.config':
+    group  => '0',
+    mode   => '644',
+    owner  => '0',
+    source => 'puppet:///modules/rabbitmq/rabbitmq.config';
+  }
+
+
+}
