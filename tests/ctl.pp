d813cbc25b30b07f8dfc27509a3926a331e867ab
Rudy Gevaert <rudy@webworm.org>
2012-01-25 15:35:51 +0100

    syntax fix

--- tests/ctl.pp
+++ tests/ctl.pp
@@ -1,4 +1,4 @@
 rabbitmq::ctl{'stop_app':}
 rabbitmq::ctl{'start_app':}
-rabbitmq::ctl{'add_user test test'}
-rabbitmq::ctl{'delete_user test'}
+rabbitmq::ctl{'add_user test test':}
+rabbitmq::ctl{'delete_user test':}
