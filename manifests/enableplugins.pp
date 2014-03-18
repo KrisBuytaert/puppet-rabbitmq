002411c0e6ba9529f2a5b3f3b56fdc0dc7d8d56b
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2014-03-18 14:05:41 +0100

    Correct exec paths and show logoutput on failure
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/enableplugins.pp
+++ manifests/enableplugins.pp
@@ -4,13 +4,14 @@ define rabbitmq::enableplugins ($plugin)
 {
 
   Exec {
-    path => ['/usr/sbin/','/bin','/usr/local/bin'],
+    path => '/usr/bin:/usr/sbin:/bin',
   }
 
   exec {"enable plugin ${plugin}":
-    command => "rabbitmq-plugins enable $plugin",
-    unless  => "rabbitmq-plugins list -E | grep $plugin",
-    notify  => Service['rabbitmq-server'],
+    command   => "rabbitmq-plugins enable $plugin",
+    unless    => "rabbitmq-plugins list -E | grep ${plugin}",
+    notify    => Service['rabbitmq-server'],
+    logoutput => 'on_failure',
   }
 
 }
