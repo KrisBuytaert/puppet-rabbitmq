c0ed634adc0e0c89ac42792a2ef85f071373eb2e
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2014-03-18 13:22:22 +0100

    Add exec path for rabbitmq-plugins
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/enableplugins.pp
+++ manifests/enableplugins.pp
@@ -3,6 +3,10 @@
 define rabbitmq::enableplugins ($plugin)
 {
 
+  Exec {
+    path => ['/usr/sbin/','/bin','/usr/local/bin'],
+  }
+
   exec {"enable plugin ${plugin}":
     command => "rabbitmq-plugins enable $plugin",
     unless  => "rabbitmq-plugins list -E | grep $plugin",
